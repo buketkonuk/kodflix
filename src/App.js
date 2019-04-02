aa2a44ac3dc19476ff76f98a2613455ca463f7d2
buketkonuk <bukikir@gmail.com>
2019-04-02 17:02:28 +0100

    add movie titles

--- src/App.js
+++ src/App.js
@@ -14,14 +14,26 @@ class App extends Component {
         <h2>Welcome to Kodflix</h2>
         <main>
           <div className='container'>
-            <div className='item'><img src={annie_hall} alt='annie_hall' /></div>
-            <div className='item'><img src={backtothefuture} alt='back tot he future' /></div>
-            <div className='item'><img src={final_destination} alt='final destination' /></div>
+            <div className='item'><img src={annie_hall} alt='annie_hall' />
+              <h1>Annie Hall</h1>
+            </div>
+            <div className='item'><img src={backtothefuture} alt='back tot he future' />
+              <h1>Back to the Future</h1>
+            </div>
+            <div className='item'><img src={final_destination} alt='final destination' />
+              <h1>Final Destination</h1>
+            </div>
           </div>
           <div className='container'>
-            <div className='item'><img src={hancock} alt='annie_hall' /></div>
-            <div className='item'><img src={obsessed} alt='obsessed' /></div>
-            <div className='item'><img src={vertigo} alt='vertigo' /></div>
+            <div className='item'><img src={hancock} alt='annie_hall' />
+              <h1>Hancock</h1>
+            </div>
+            <div className='item'><img src={obsessed} alt='obsessed' />
+              <h1>Obsessed</h1>
+            </div>
+            <div className='item'><img src={vertigo} alt='vertigo' />
+              <h1>Vertigo</h1>
+            </div>
           </div>
         </main>
       </div>
