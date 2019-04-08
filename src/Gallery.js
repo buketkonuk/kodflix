d7e4360de70171065373d98a535a7eb49832e6f0
buketkonuk <bukikir@gmail.com>
2019-04-08 10:55:44 +0100

    task10 create unique page for each tv show

--- src/Gallery.js
+++ src/Gallery.js
@@ -12,14 +12,14 @@ export default function Gallery() {
         <main>
             <h2>Welcome to Kodflix</h2>
             <div className='container'>
-                <TvShow name="Annie Hall" image={annie_hall} />
-                <TvShow name="Back to the Future" image={backtothefuture} />
-                <TvShow name="Final Destination" image={final_destination} />
+                <TvShow id='annie-hall' name="Annie Hall" image={annie_hall} />
+                <TvShow id= 'back-to-the-future' name="Back to the Future" image={backtothefuture} />
+                <TvShow id= 'final-destination' name="Final Destination" image={final_destination} />
             </div>
             <div className='container'>
-                <TvShow name="Hancock Hall" image={hancock} />
-                <TvShow name="Obsessed Hall" image={obsessed} />
-                <TvShow name="Vertigo" image={vertigo} />
+                <TvShow id = 'hancock' name="Hancock" image={hancock} />
+                <TvShow id= 'obsessed' name="Obsessed" image={obsessed} />
+                <TvShow id= 'vertigo' name="Vertigo" image={vertigo} />
 
             </div>
         </main>
--- src/TvShow.js
+++ src/TvShow.js
@@ -4,7 +4,7 @@ import {Link} from 'react-router-dom';
 export default function TvShow(props) {
     return (
 
-        <Link to="/details" className='item'><img src={props.image} alt={`${props.name}`} />
+        <Link to={"/" + props.id} className='item'><img src={props.image} alt={`${props.name}`} />
             <div className='overlay'>
                 <h2>{props.name}</h2>
             </div>
