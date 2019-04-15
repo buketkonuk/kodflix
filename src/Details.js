e708b9e37e973de8375beb8389d51a2ebb6f2103
buketkonuk <bukikir@gmail.com>
2019-04-15 16:13:00 +0100

    add synopsis page for each movie

--- src/Details.js
+++ src/Details.js
@@ -1,5 +1,5 @@
 import React from 'react';
-// import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import getTvShow from './getTvShow.js';
 // import NotFound from './NotFound';
 import { Redirect } from 'react-router-dom';
@@ -7,39 +7,35 @@ import { Redirect } from 'react-router-dom';
 export default class Details extends React.Component {
 
   constructor() {
-    super(); 
-      this.state = {
-        tvShow: {}
-      };
-    }
-  
+    super();
+    this.state = {
+      tvShow: {}
+    };
+  }
+
   componentDidMount() {
     let tvShowId = this.props.match.params.tvShowId;
     let tvShow = getTvShow().find(show => show.id === tvShowId);
 
-    this.setState( {tvShow });
- 
- 
- 
-
+    this.setState({ tvShow });
 
   }
   render() {
     let movie = this.state.tvShow;
     if (movie) {
-      return <h1>{this.state.tvShow.name}</h1>;
+      return (
+        <div>
+          <Link to="/" > Back to HomePage </Link>
+          <h1>{movie.name}</h1>
+          <div className='container'>
+            <h2>{movie.synopsis}</h2>
+            <div className ='item'><img src={movie.image} alt={`${movie.name}`} /></div>
+          </div>
+        </div>
+      );
     }
     else {
       return <Redirect to='/not-found' />;
     }
-
-    // return (
-    //   <div>
-    //     <Link to='/'>Back to Home Page</Link>
-    //     <h1>{this.state.tvShow.name}</h1>
-        
-    //   </div>
-    // );
   }
-
 }
--- src/getTvShow.js
+++ src/getTvShow.js
@@ -8,12 +8,12 @@ import vertigo from "./img/vertigo.jpg";
 
 export default function getTvShow() {
     return [
-       {id:'annie-hall', name: "Annie Hall", image: annie_hall},
-       {id:'back-to-the-future', name: "Back to the Future", image: backtothefuture},
-       {id:'final-destination', name: "Final Destination", image: final_destination},
-       {id:'hancock', name: "Hancock", image: hancock},
-       {id:'obsessed', name: "Obsessed", image: obsessed},
-       {id:'vertigo', name: "Vertigo", image: vertigo},
+       {id:'annie-hall', name: "Annie Hall", image: annie_hall, synopsis:"Another one of Woody Allen's sexual sufferings"},
+       {id:'back-to-the-future', name: "Back to the Future", image: backtothefuture, synopsis:"The guy got Parkinsons for God's sake"},
+       {id:'final-destination', name: "Final Destination", image: final_destination, synopsis:"If it's you time to die, you should die"},
+       {id:'hancock', name: "Hancock", image: hancock, synopsis:"What kind of a supernatural hero movie is this?? I think it's shit but my brother likes it"},
+       {id:'obsessed', name: "Obsessed", image: obsessed, synopsis:"Things couldn't be better for Derek Charles (Idris Elba). He's just received a big promotion at work, and has a wonderful marriage with his beautiful wife, Sharon (Beyoncé Knowles)"},
+       {id:'vertigo', name: "Vertigo", image: vertigo, synopsis:"Another fantastic psyco thriller from the master"},
     ];
   
   }
