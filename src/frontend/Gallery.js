e45ac95373bb8bd30c5bf3c3c730be06b7c9d51b
buketkonuk <bukikir@gmail.com>
2019-04-24 11:21:19 +0100

    introduce a backend server

---
+++ src/frontend/Gallery.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import getTvShow from './getTvShow'
+import TvShow from "./TvShow";
+
+export default function Gallery() {
+    return (
+        <main>
+            <h2>Welcome to Buket's Kodflix</h2>
+            <div className='container'>
+            {
+                getTvShow().map(tvShow => (
+                    <TvShow 
+                    key={tvShow.id}
+                    id={tvShow.id} 
+                    name={tvShow.name} 
+                    image={tvShow.image} />
+                ))
+            }    
+            </div>
+        </main>
+    );
+
+}
