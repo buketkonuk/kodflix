3e928b3d3aa624f2ef4fe66782b88863123882f7
buketkonuk <bukikir@gmail.com>
2019-04-25 17:09:00 +0100

    task19 send the list of shows in backend

--- src/backend/app.js
+++ src/backend/app.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const showsList = require("./showsList.js");
 
-app.get('/', (req, res) => res.send('Hello World!'))
+
+app.get("/rest/shows", (req, res) => {
+    res.send(showsList);
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
