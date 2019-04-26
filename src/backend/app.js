9440f74a90ec08a0c8cc1e03ad02b57c268053fe
buketkonuk <bukikir@gmail.com>
2019-04-26 11:18:12 +0100

    add the front page greeting to backend start

--- src/backend/app.js
+++ src/backend/app.js
@@ -7,5 +7,7 @@ const showsList = require("./showsList.js");
 app.get("/rest/shows", (req, res) => {
     res.send(showsList);
 })
-
+app.get("/", (req, res) => {
+    res.send('Welcome to Buket Kodflix App Front Page')
+})
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
