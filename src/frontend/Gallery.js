c7cb6fafa0601aa4201eec9aae4ffdfe45acbd72
buketkonuk <bukikir@gmail.com>
2019-04-29 16:41:24 +0100

    add fetch api from backend

--- src/frontend/Gallery.js
+++ src/frontend/Gallery.js
@@ -1,8 +1,18 @@
 import React from 'react';
-import getTvShow from './getTvShow'
+import getTvShow from './getTvShow';
 import TvShow from "./TvShow";
 
-export default function Gallery() {
+
+
+export default class Gallery extends React.Component {
+    componentDidMount() {
+        fetch("/rest/shows")
+            .then(res => res.json())
+            .then(ShowsList => this.setState({ShowsList}));
+      }
+
+
+    render() {
     return (
         <main>
             <h2>Welcome to Buket's Kodflix</h2>
@@ -20,4 +30,5 @@ export default function Gallery() {
         </main>
     );
 
+}
 }
