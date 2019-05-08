3bd25349dc7b9941ed0ce050e9ca3f416efb0269
buketkonuk <bukikir@gmail.com>
2019-05-08 12:44:53 +0100

    more

--- src/backend/app.js
+++ src/backend/app.js
@@ -4,10 +4,10 @@ const port = process.env.PORT || 3001;
 const showsList = require("./showsList.js");
 const path = require('path');
 
-app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.static(path.join(__dirname, './../../build')));
 
 app.get('*', function (req, res) {
-    res.sendFile(path.join(__dirname, '../public', 'index.html'));
+    res.sendFile(path.join(__dirname, "./../../build", 'index.html'));
 });
 
 app.get("/rest/shows", (req, res) => {
