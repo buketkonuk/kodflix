447bb0c84c348f6fc3ca0eee0523ff0f145b5a0c
buketkonuk <bukikir@gmail.com>
2019-05-08 15:32:58 +0100

    changes for backend

--- src/backend/showsList.js
+++ src/backend/showsList.js
@@ -1,12 +1,13 @@
-const showsList= [
-{"id":'annie-hall', 
-    "title": "Annie Hall",  
-    "synopsis":"Another one of Woody Allen's sexual sufferings"
-},
-{"id":'back-to-the-future', "title": "Back to the Future", "synopsis":"The guy got Parkinsons for God's sake"},
-{"id":'final-destination', "title": "Final Destination",  "synopsis":"If it's you time to die, you should die"},
-{"id":'hancock', "title": "Hancock",  "synopsis":"What kind of a supernatural hero movie is this?? I think it's shit but my brother likes it"},
-{"id":'obsessed', "title": "Obsessed", "synopsis":"Things couldn't be better for Derek Charles (Idris Elba). He's just received a big promotion at work, and has a wonderful marriage with his beautiful wife, Sharon (Beyoncé Knowles)"},
-{"id":'vertigo', "title": "Vertigo", "synopsis":"Another fantastic psyco thriller from the master"},
-]
-module.exports = showsList;
+module.exports = function showsList() {
+    return [
+        {"id":'annie-hall', 
+            "title": "Annie Hall",  
+            "synopsis":"Another one of Woody Allen's sexual sufferings"
+        },
+        {"id":'back-to-the-future', "title": "Back to the Future", "synopsis":"The guy got Parkinsons for God's sake"},
+        {"id":'final-destination', "title": "Final Destination",  "synopsis":"If it's you time to die, you should die"},
+        {"id":'hancock', "title": "Hancock",  "synopsis":"What kind of a supernatural hero movie is this?? I think it's shit but my brother likes it"},
+        {"id":'obsessed', "title": "Obsessed", "synopsis":"Things couldn't be better for Derek Charles (Idris Elba). He's just received a big promotion at work, and has a wonderful marriage with his beautiful wife, Sharon (Beyoncé Knowles)"},
+        {"id":'vertigo', "title": "Vertigo", "synopsis":"Another fantastic psyco thriller from the master"}
+        ];
+} 
