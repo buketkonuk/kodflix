0b9585fa310388f329dc95353d92f48167ecafc4
buketkonuk <bukikir@gmail.com>
2019-05-08 16:00:42 +0100

    changes for backend

--- src/backend/app.js
+++ src/backend/app.js
@@ -1,18 +1,19 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3001;
-const showsList = require("./showsList.js");
+const showsList = require("./showsList");
 const path = require('path');
 
+
+app.get("/rest/shows", (req, res) => {
+    res.send(showsList());
+})
 app.use(express.static(path.join(__dirname, './../../build')));
 
 app.get('*', function (req, res) {
     res.sendFile(path.join(__dirname, "./../../build", 'index.html'));
 });
 
-app.get("/rest/shows", (req, res) => {
-    res.send(showsList);
-})
 app.get("/", (req, res) => {
     res.send('Welcome to Buket Kodflix App Front Page')
 })
