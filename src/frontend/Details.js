9adbd5a88ad1595c6166c8a07a2debf5850ccfac
Buket <bukikir@gmail.com>
2019-05-14 16:58:10 +0100

    many changes done for task24 (#30)

--- src/frontend/Details.js
+++ src/frontend/Details.js
@@ -1,41 +1,61 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import getTvShow from './getTvShow.js';
 // import NotFound from './NotFound';
 import { Redirect } from 'react-router-dom';
 
 export default class Details extends React.Component {
-
+// first again get empty array state
+// CDM data will come from fetch api (rest/shows)
+// fetch then- showList.find(matching url)
+// in the render, try to use the data from backend showsList like in TvShow by using this.state.name
+// instead do state destructuring
   constructor() {
     super();
     this.state = {
-      tvShow: {}
+      tvShow: []
     };
   }
 
   componentDidMount() {
-    let tvShowId = this.props.match.params.tvShowId;
-    let tvShow = getTvShow().find(show => show.id === tvShowId);
-
-    this.setState({ tvShow });
-
+    fetch("/rest/shows")
+      .then(res => res.json())
+      .then(ShowsList => {
+        let tvShowId = this.props.match.params.tvShowId;
+        // console.log(tvShowId);
+        let tvShow = ShowsList.find(show => show.id === tvShowId);
+        // console.log(tvShow);
+        this.setState({ tvShow : tvShow}, () => console.log("new state",this.state.tvShow));
+        // console.log(this.state.tvShow.id)
+      });
   }
   render() {
     let movie = this.state.tvShow;
+    console.log(movie.id);
     if (movie) {
-      return (
-        <div>
-          <Link to="/" > Back to HomePage </Link>
-          <h1>{movie.name}</h1>
-          <div className='container'>
-            <h2>{movie.synopsis}</h2>
-            <div className ='item'><img src={movie.image} alt={`${movie.name}`} /></div>
-          </div>
-        </div>
-      );
+      return movie.id ? 
+      <DetailsOfTvShow TvShow={movie} /> :
+      <div></div>
     }
     else {
       return <Redirect to='/not-found' />;
     }
   }
 }
+  function DetailsOfTvShow({ TvShow }) {
+
+    return (
+      <div>
+      <Link to="/" > Back to HomePage </Link>
+      <h1>{TvShow.title}</h1>
+      <div className='container'>
+        <h2>{TvShow.synopsis}</h2>
+        <div className ='item'>
+        <img src={require(`./img/${TvShow.id}.jpg`)} alt={TvShow.title} />
+        </div>
+      </div>
+      </div>
+    );
+  }
+
+
+
--- src/frontend/TvShow.js
+++ src/frontend/TvShow.js
@@ -4,9 +4,9 @@ import {Link} from 'react-router-dom';
 export default function TvShow(props) {
     return (
 
-        <Link to={"/" + props.id} className='item'><img src={props.image} alt={`${props.name}`} />
+        <Link to={"/" + props.id} className='item'><img src={require(`../frontend/img/${props.id}.jpg`)} alt={`${props.title}`} />
             <div className='overlay'>
-                <h2>{props.name}</h2>
+                <h2>{props.title}</h2>
             </div>
         </Link>
 
